fcce2b9121deb2112f509ea5010551fb0e80e4e2
ayush pathak <ayushpathak1975@gmail.com>
2024-12-17 15:36:32 +0530

    todays commit is here

--- src/main/java/collection/LinkedListQueue.java
+++ src/main/java/collection/LinkedListQueue.java
@@ -6,10 +6,14 @@ import java.util.Queue;
 public class LinkedListQueue {
     public static void main(String[] args) {
         Queue<Integer> lq=new LinkedList<>();
+
+
         lq.offer(22);
         lq.add(10);
         lq.offer(15);
         lq.add(19);
+
+
 
         System.out.println(lq);
 
